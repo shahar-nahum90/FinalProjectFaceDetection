6b533301a7eb52edd066149238cc93a47384e3d9
Shahar_Nahum <62279310+shahar-nahum90@users.noreply.github.com>
2021-06-17 13:53:12 +0300

    Add files via upload

---
+++ javaPoject/VisionProcessorBase.java
@@ -0,0 +1,283 @@
+package com.google.mlkit.vision.demo.java;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import android.app.ActivityManager;
+import android.app.ActivityManager.MemoryInfo;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build.VERSION_CODES;
+import android.os.SystemClock;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.GuardedBy;
+import androidx.camera.core.ExperimentalGetImage;
+import androidx.camera.core.ImageProxy;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.demo.BitmapUtils;
+import com.google.mlkit.vision.demo.CameraImageGraphic;
+import com.google.mlkit.vision.demo.FrameMetadata;
+import com.google.mlkit.vision.demo.GraphicOverlay;
+import com.google.mlkit.vision.demo.InferenceInfoGraphic;
+import com.google.mlkit.vision.demo.ScopedExecutor;
+import com.google.mlkit.vision.demo.VisionImageProcessor;
+import com.google.mlkit.vision.demo.preference.PreferenceUtils;
+import java.nio.ByteBuffer;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public abstract class VisionProcessorBase<T> implements VisionImageProcessor {
+
+  protected static final String MANUAL_TESTING_LOG = "LogTagForTest";
+  private static final String TAG = "VisionProcessorBase";
+
+  private final ActivityManager activityManager;
+  private final Timer fpsTimer = new Timer();
+  private final ScopedExecutor executor;
+
+  private boolean isShutdown;
+
+  private int numRuns = 0;
+  private long totalFrameMs = 0;
+  private long maxFrameMs = 0;
+  private long minFrameMs = Long.MAX_VALUE;
+  private long totalDetectorMs = 0;
+  private long maxDetectorMs = 0;
+  private long minDetectorMs = Long.MAX_VALUE;
+
+  // Frame count that have been processed so far in an one second interval to calculate FPS.
+  private int frameProcessedInOneSecondInterval = 0;
+  private int framesPerSecond = 0;
+
+  // To keep the latest images and its metadata.
+  @GuardedBy("this")
+  private ByteBuffer latestImage;
+
+  @GuardedBy("this")
+  private FrameMetadata latestImageMetaData;
+  // To keep the images and metadata in process.
+  @GuardedBy("this")
+  private ByteBuffer processingImage;
+
+  @GuardedBy("this")
+  private FrameMetadata processingMetaData;
+
+  protected VisionProcessorBase(Context context) {
+    activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    executor = new ScopedExecutor(TaskExecutors.MAIN_THREAD);
+    fpsTimer.scheduleAtFixedRate(
+        new TimerTask() {
+          @Override
+          public void run() {
+            framesPerSecond = frameProcessedInOneSecondInterval;
+            frameProcessedInOneSecondInterval = 0;
+          }
+        },
+        /* delay= */ 0,
+        /* period= */ 1000);
+  }
+
+
+  @Override
+  public void processBitmap(Bitmap bitmap, final GraphicOverlay graphicOverlay) {
+    long frameStartMs = SystemClock.elapsedRealtime();
+
+    requestDetectInImage(
+        InputImage.fromBitmap(bitmap, 0),
+        graphicOverlay,
+        /* originalCameraImage= */ null,
+        /* shouldShowFps= */ false,
+        frameStartMs);
+  }
+
+
+  @Override
+  public synchronized void processByteBuffer(
+      ByteBuffer data, final FrameMetadata frameMetadata, final GraphicOverlay graphicOverlay) {
+    latestImage = data;
+    latestImageMetaData = frameMetadata;
+    if (processingImage == null && processingMetaData == null) {
+      processLatestImage(graphicOverlay);
+    }
+  }
+
+  private synchronized void processLatestImage(final GraphicOverlay graphicOverlay) {
+    processingImage = latestImage;
+    processingMetaData = latestImageMetaData;
+    latestImage = null;
+    latestImageMetaData = null;
+    if (processingImage != null && processingMetaData != null && !isShutdown) {
+      processImage(processingImage, processingMetaData, graphicOverlay);
+    }
+  }
+
+  private void processImage(
+      ByteBuffer data, final FrameMetadata frameMetadata, final GraphicOverlay graphicOverlay) {
+    long frameStartMs = SystemClock.elapsedRealtime();
+
+
+    Bitmap bitmap =
+        PreferenceUtils.isCameraLiveViewportEnabled(graphicOverlay.getContext())
+            ? null
+            : BitmapUtils.getBitmap(data, frameMetadata);
+
+    requestDetectInImage(
+            InputImage.fromByteBuffer(
+                data,
+                frameMetadata.getWidth(),
+                frameMetadata.getHeight(),
+                frameMetadata.getRotation(),
+                InputImage.IMAGE_FORMAT_NV21),
+            graphicOverlay,
+            bitmap,
+            /* shouldShowFps= */ true,
+            frameStartMs)
+        .addOnSuccessListener(executor, results -> processLatestImage(graphicOverlay));
+  }
+
+  @Override
+  @RequiresApi(VERSION_CODES.KITKAT)
+  @ExperimentalGetImage
+  public void processImageProxy(ImageProxy image, GraphicOverlay graphicOverlay) {
+    long frameStartMs = SystemClock.elapsedRealtime();
+    if (isShutdown) {
+      image.close();
+      return;
+    }
+
+    Bitmap bitmap = null;
+    if (!PreferenceUtils.isCameraLiveViewportEnabled(graphicOverlay.getContext())) {
+      bitmap = BitmapUtils.getBitmap(image);
+    }
+
+    requestDetectInImage(
+            InputImage.fromMediaImage(image.getImage(), image.getImageInfo().getRotationDegrees()),
+            graphicOverlay,
+            /* originalCameraImage= */ bitmap,
+            /* shouldShowFps= */ true,
+            frameStartMs)
+
+        .addOnCompleteListener(results -> image.close());
+  }
+
+  private Task<T> requestDetectInImage(
+      final InputImage image,
+      final GraphicOverlay graphicOverlay,
+      @Nullable final Bitmap originalCameraImage,
+      boolean shouldShowFps,
+      long frameStartMs) {
+    return setUpListener(
+        detectInImage(image), graphicOverlay, originalCameraImage, shouldShowFps, frameStartMs);
+  }
+
+  private Task<T> setUpListener(
+      Task<T> task,
+      final GraphicOverlay graphicOverlay,
+      @Nullable final Bitmap originalCameraImage,
+      boolean shouldShowFps,
+      long frameStartMs) {
+    final long detectorStartMs = SystemClock.elapsedRealtime();
+    return task.addOnSuccessListener(
+            executor,
+            results -> {
+              long endMs = SystemClock.elapsedRealtime();
+              long currentFrameLatencyMs = endMs - frameStartMs;
+              long currentDetectorLatencyMs = endMs - detectorStartMs;
+              if (numRuns >= 500) {
+                resetLatencyStats();
+              }
+              numRuns++;
+              frameProcessedInOneSecondInterval++;
+              totalFrameMs += currentFrameLatencyMs;
+              maxFrameMs = max(currentFrameLatencyMs, maxFrameMs);
+              minFrameMs = min(currentFrameLatencyMs, minFrameMs);
+              totalDetectorMs += currentDetectorLatencyMs;
+              maxDetectorMs = max(currentDetectorLatencyMs, maxDetectorMs);
+              minDetectorMs = min(currentDetectorLatencyMs, minDetectorMs);
+
+              if (frameProcessedInOneSecondInterval == 1) {
+                Log.d(TAG, "Num of Runs: " + numRuns);
+                Log.d(
+                    TAG,
+                    "Frame latency: max="
+                        + maxFrameMs
+                        + ", min="
+                        + minFrameMs
+                        + ", avg="
+                        + totalFrameMs / numRuns);
+                Log.d(
+                    TAG,
+                    "Detector latency: max="
+                        + maxDetectorMs
+                        + ", min="
+                        + minDetectorMs
+                        + ", avg="
+                        + totalDetectorMs / numRuns);
+                MemoryInfo mi = new MemoryInfo();
+                activityManager.getMemoryInfo(mi);
+                long availableMegs = mi.availMem / 0x100000L;
+                Log.d(TAG, "Memory available in system: " + availableMegs + " MB");
+              }
+
+              graphicOverlay.clear();
+              if (originalCameraImage != null) {
+                graphicOverlay.add(new CameraImageGraphic(graphicOverlay, originalCameraImage));
+              }
+              VisionProcessorBase.this.onSuccess(results, graphicOverlay);
+              if (!PreferenceUtils.shouldHideDetectionInfo(graphicOverlay.getContext())) {
+                graphicOverlay.add(
+                    new InferenceInfoGraphic(
+                        graphicOverlay,
+                        currentFrameLatencyMs,
+                        currentDetectorLatencyMs,
+                        shouldShowFps ? framesPerSecond : null));
+              }
+              graphicOverlay.postInvalidate();
+            })
+        .addOnFailureListener(
+            executor,
+            e -> {
+              graphicOverlay.clear();
+              graphicOverlay.postInvalidate();
+              String error = "Failed to process. Error: " + e.getLocalizedMessage();
+              Toast.makeText(
+                      graphicOverlay.getContext(),
+                      error + "\nCause: " + e.getCause(),
+                      Toast.LENGTH_SHORT)
+                  .show();
+              Log.d(TAG, error);
+              e.printStackTrace();
+              VisionProcessorBase.this.onFailure(e);
+            });
+  }
+
+  @Override
+  public void stop() {
+    executor.shutdown();
+    isShutdown = true;
+    resetLatencyStats();
+    fpsTimer.cancel();
+  }
+
+  private void resetLatencyStats() {
+    numRuns = 0;
+    totalFrameMs = 0;
+    maxFrameMs = 0;
+    minFrameMs = Long.MAX_VALUE;
+    totalDetectorMs = 0;
+    maxDetectorMs = 0;
+    minDetectorMs = Long.MAX_VALUE;
+  }
+
+  protected abstract Task<T> detectInImage(InputImage image);
+
+  protected abstract void onSuccess(@NonNull T results, @NonNull GraphicOverlay graphicOverlay);
+
+  protected abstract void onFailure(@NonNull Exception e);
+}
